241852c0eda721c76ffcd6a2f0a9c21535d19177
Endre Berki <berkiendre@gmail.com>
2020-05-03 00:23:17 +0200

    Add part0 solutions

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+
+
 const App = (props) => {
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(new Array(props.anecdotes.length).fill(0))

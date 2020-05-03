684c45352cb82c4d56be7b75926dc5e64aba2096
Endre Berki <berkiendre@gmail.com>
2020-05-03 14:10:18 +0200

    Part 2.1-2.3

---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Total = ({ numbers }) => {
+  const sum = numbers.reduce((sum, number) => sum + number, 0);
+  return(
+    <p>total of {sum} exercises</p>
+  ) 
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>    
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map(coursePart => 
+                          <Part key={coursePart.id} part={coursePart} />
+                       )}
+    </div>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <>
+      <Header course={course} />
+      <Content course={course} />
+      <b>
+        <Total numbers={course.parts.map(coursePart => coursePart.exercises)} />
+      </b>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))

17bf8372bd6a84b9d272be87f43bd89fb57bf4ad
Endre Berki <berkiendre@gmail.com>
2020-05-04 14:06:22 +0200

    2.15 - 2.18 - Altering data on the server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
-import axios from 'axios'
+import PersonsService from './services/PersonsService'
 
 const App = () => {
   const [ persons, setPersons ] = useState([]) 
@@ -13,28 +13,45 @@ const App = () => {
   const addName = (event) => {
     event.preventDefault();
 
-    if(persons.map(p => p.name).includes(newName))
-    {
-      alert(`${newName} is already added to phonebook`)
-      return;
-    }
-
-    let newPerson = {
+    let newOrUpdatedPerson = {
       name: newName,
       number: newNumber
     };
 
-    setPersons(persons.concat(newPerson));
+    if(persons.map(p => p.name).includes(newName))
+    {
+      let alreadyAddedPerson = persons.find(p => p.name === newName)
+
+      if(window.confirm(`${alreadyAddedPerson.name} is already added to the phonebook, replace the old number with a new one?`)) {
+        PersonsService.updatePerson(alreadyAddedPerson.id, newOrUpdatedPerson)
+                      .then(updatedPerson => setPersons(persons.map(p => p.id !== updatedPerson.id ? p : updatedPerson)))
+      }
+      else {
+        return;
+      }
+    }
+    else {
+      PersonsService.addPerson(newOrUpdatedPerson)
+                    .then(createdPerson => setPersons(persons.map(p => p.id !== createdPerson.id ? p : createdPerson)))
+    }
+
     setNewName('');
     setNewNumber('');
   }
 
+  const handleDelete = event => {
+      let idOfDeletedPerson = Number(event.target.id);
+
+      if(window.confirm(`Delete ${persons.find(p => p.id === idOfDeletedPerson).name}?`)) {
+        PersonsService.deletePerson(idOfDeletedPerson)
+                      .then(setPersons(persons.filter(p => p.id !== idOfDeletedPerson)))
+                      .catch(error => alert(`unsuccessful delete: ${error}`))
+      }
+  }
+
   useEffect(() => {
-    axios
-    .get('http://localhost:3001/persons')
-    .then(response => {
-      setPersons(response.data)
-    })
+    PersonsService.getAll()
+                .then(persons => setPersons(persons))
   })
 
   return (
@@ -50,7 +67,7 @@ const App = () => {
                     submitHandler={addName}
                     />
       <h2>Numbers</h2>
-          <Persons persons={persons} nameFilter={nameFilter} />
+          <Persons persons={persons} nameFilter={nameFilter} deleteButtonHandler={handleDelete} />
     </div>
   )
 }
---
+++ part2/phonebook/src/services/PersonsService.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/persons'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const addPerson = newObject => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const deletePerson = id => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(response => response.data)
+}
+
+const updatePerson = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+export default { getAll, addPerson, updatePerson, deletePerson }

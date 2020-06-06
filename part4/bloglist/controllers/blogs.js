113371bb1ae113526bae4352f0fab1a09c221024
Endre Berki <berkiendre@gmail.com>
2020-06-06 21:56:56 +0200

    4.8 - Introduce node environment, use supertest for itests, use async/await

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,12 +1,14 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogsRouter.get('/', async (request, response, next) => {
+    try {
+        const blogs = await Blog.find({})
+        response.json(blogs.map(blog => blog.toJSON()))
+    }
+    catch(exception) {
+        next(exception)
+    }
 })
 
 blogsRouter.post('/', (request, response) => {
--- part4/bloglist/utils/logger.js
+++ part4/bloglist/utils/logger.js
@@ -1,11 +1,13 @@
 const info = (...params) => {
-    console.log(...params)
-  }
+    if (process.env.NODE_ENV !== 'test') { 
+        console.log(...params)
+    }
+}
   
-  const error = (...params) => {
+const error = (...params) => {
     console.error(...params)
-  }
+}
   
-  module.exports = {
+module.exports = {
     info, error
-  }
+}

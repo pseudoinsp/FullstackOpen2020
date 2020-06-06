e4b6627d9ca84c18896bf26ee4d2e24dbfe8195a
Endre Berki <berkiendre@gmail.com>
2020-06-06 17:52:50 +0200

    4.5 - Calculate favorite blog and utests

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -7,8 +7,15 @@ const totalLikes = (blogs) => {
     const reducer = (sum, item) => sum + item
     return blogs.length === 0 ? 0 : blogs.map(blog => blog.likes).reduce(reducer, 0)
 }
+
+const favoriteBlog = (blogs) => {
+    const reducer = (withMaxLike, current) => withMaxLike.likes >= current.likes ?  withMaxLike : current
+
+    return blogs.length === 0 ? null : blogs.reduce(reducer, blogs[0])
+}
   
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }

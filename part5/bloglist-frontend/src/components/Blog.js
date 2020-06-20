8db49513ff190299b3bc796f307cc5e0627ef6a4
Endre Berki <berkiendre@gmail.com>
2020-06-20 17:48:30 +0200

    5.7 - Support viewing/hiding detailed data about individual blogs

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,33 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [detailed, setDetailed] = useState(false) 
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if(detailed) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} <button onClick={() => setDetailed(false)}>hide</button>
+        <br />
+        {blog.url}
+        <br />
+        likes {blog.likes} <button>like</button>
+        <br />
+        {blog.author}
+      </div>
+    )
+  }
+
+  return (
+    <div blogStyle={blogStyle}>
+      {blog.title} {blog.author} <button onClick={() => setDetailed(true)}>view</button>
+    </div>
+  )}
 
 export default Blog

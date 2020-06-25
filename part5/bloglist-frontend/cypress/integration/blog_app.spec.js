4917743c2881e874a9d3db790b9c11cff94f89bb
Endre Berki <berkiendre@gmail.com>
2020-06-26 00:07:54 +0200

    5.19 - Testing creation of new blog

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,4 +1,4 @@
-describe('Note app', function() {
+describe('Blog app', function() {
     beforeEach(function() {
         cy.request('POST', 'http://localhost:3001/api/testing/reset')
         const user = {
@@ -25,7 +25,7 @@ describe('Note app', function() {
             cy.contains('logged in')
         })
     
-        it.only('fails with wrong credentials', function() {
+        it('fails with wrong credentials', function() {
             cy.get('#username').type('testelek')
             cy.get('#password').type('alma123')
             cy.get('#login-button').click()
@@ -33,6 +33,28 @@ describe('Note app', function() {
             cy.get('.error')
                 .contains('Unsuccessful login')
                 .and('have.css', 'color', 'rgb(255, 0, 0)')
+        })
+      })
+
+      describe.only('When logged in', function() {
+        beforeEach(function() {
+            cy.get('#username').type('testelek')
+            cy.get('#password').type('helloka')
+            cy.get('#login-button').click()
+        
+            cy.contains('logged in')
+        })
+    
+        it('A blog can be created', function() {
+            cy.contains('new blog').click()
+
+            cy.get('#title').type('testTitle')
+            cy.get('#author').type('testAuthor')
+            cy.get('#url').type('testUrl')
+
+            cy.contains('create').click()
+
+            cy.contains('testAuthor')
         })
       })
   })              

04ded7e0921114b7a7237e4475f332070befb9a0
endre.berki <gonosztaviszorny@gmail.com>
2020-07-09 19:08:48 +0200

    8.12 - On author edition, only known authors can be selected for edit

--- part8/library-frontend/src/components/EditAuthor.js
+++ part8/library-frontend/src/components/EditAuthor.js
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from 'react'
-import { ALL_AUTHORS, OVERWRITE_AUTHOR } from '../queries'
-import { useMutation } from '@apollo/client'
+import { ALL_AUTHORS, ALL_AUTHOR_NAMES, OVERWRITE_AUTHOR } from '../queries'
+import { useQuery, useMutation } from '@apollo/client'
+import Select  from 'react-select'
 
 const EditAuthor = (props) => {
-    const [authorName, setAuthorName] = useState('')
+    const [authorName, setAuthorName] = useState({})
     const [birthYear, setBirthYear] = useState('')
+    const [options, setOptions] = useState([])
+
+    useQuery(ALL_AUTHOR_NAMES, {
+        onCompleted: data => {
+          const authors = data.allAuthors
+          const newOptions = authors.map(a => {
+            return { label: a.name, value: a.name }
+          })
+          setOptions(newOptions)
+    }})
   
     const [ overwriteAuthor, result ] = useMutation(OVERWRITE_AUTHOR, {
       refetchQueries: [  {query: ALL_AUTHORS} ],
-      onError: (error) => {
+      onError: (error) => { 
         console.log(JSON.stringify(error, null, 2));
       },
     })
@@ -26,21 +37,20 @@ const EditAuthor = (props) => {
     const submit = async (event) => {
         event.preventDefault()
 
-        overwriteAuthor({ variables: { name: authorName, setBornTo: Number(birthYear) } })
+        overwriteAuthor({ variables: { name: authorName.value, setBornTo: Number(birthYear) } })
   
         setAuthorName('')
         setBirthYear('')
     }
-  
+
     return (
       <div>
         <form onSubmit={submit}>
           <div>
             name
-            <input
-              value={authorName}
-              onChange={({ target }) => setAuthorName(target.value)}
-            />
+            <Select options={options} 
+                    value={authorName}
+                    onChange={( selected ) => { console.log(selected); setAuthorName(selected)}} />
           </div>
           <div>
             born
--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -10,6 +10,14 @@ query {
   }
   `
 
+  export const ALL_AUTHOR_NAMES = gql`
+  query {
+      allAuthors  {
+        name
+      }
+    }
+  `
+
   export const ALL_BOOKS = gql`
   query {
       allBooks  {

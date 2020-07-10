788571af8f41fcd052b6c5e545c8deb1b1d2a0e3
endre.berki <gonosztaviszorny@gmail.com>
2020-07-11 00:16:01 +0200

    8.18 - Add user login to FE

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,20 +1,45 @@
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import EditAuthor from './components/EditAuthor'
+import LoginForm from './components/LoginForm'
+import { useApolloClient } from '@apollo/client'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  const [token, setToken] = useState(null)
+
+  const client = useApolloClient()
+
+  const handleLogin = receivedToken => {
+    setToken(receivedToken)
+    localStorage.setItem('library-user-token', receivedToken)
+  }
+
+  const handleLogout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  useEffect(() => {
+    const persistedToken = window.localStorage.getItem('library-user-token')
+    if (persistedToken) {
+        setToken(persistedToken)
+    }
+}, [])
 
   return (
     <div>
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
-        <button onClick={() => setPage('editAuthor')}>edit author</button>
+        {token && <button onClick={() => setPage('add')}>add book</button>}
+        {token && <button onClick={() => setPage('editAuthor')}>edit author</button>}
+        {!token && <button onClick={() => setPage('login')}>login</button>}
+        {token && <button onClick={() => handleLogout()}>logout</button>}
       </div>
 
       <Authors
@@ -32,6 +57,10 @@ const App = () => {
       <EditAuthor
         show={page === 'editAuthor'}
       />
+
+      <LoginForm handleLogin={handleLogin}
+        show={page === 'login'}
+      />    
 
     </div>
   )
--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -54,3 +54,11 @@ export const OVERWRITE_AUTHOR = gql`
     }
 }
 `
+
+export const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password)  {
+      value
+    }
+  }
+`

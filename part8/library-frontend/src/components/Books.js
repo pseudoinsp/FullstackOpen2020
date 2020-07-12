cf34775f14aedb54344e727e3e9d3e4b16c64f92
endre.berki <gonosztaviszorny@gmail.com>
2020-07-12 16:47:47 +0200

    8.25 - Update client cache when notified about new book addition

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,21 +1,53 @@
 import React, { useEffect, useState } from 'react'
 import { ALL_BOOKS, ALL_GENRES, ALL_BOOKS_WITH_GENRE, BOOK_ADDED } from '../queries' 
-import { useQuery, useLazyQuery, useSubscription } from '@apollo/client'
+import { useQuery, useLazyQuery, useSubscription, useApolloClient } from '@apollo/client'
 
 const Books = (props) => {
-  const booksQueryResult = useQuery(ALL_BOOKS, {
-    onCompleted: data => {
-      setBooks(data.allBooks)
-    }
-  })
+  // order is important here, if ALL_GENRES would come later, the cache would contain only genres info about books
   const genresQueryResult = useQuery(ALL_GENRES)
+  const booksQueryResult = useQuery(ALL_BOOKS)
   const [ bookByGenres, bookByGenreResult ] = useLazyQuery(ALL_BOOKS_WITH_GENRE)
   const [books, setBooks] = useState([])
   const [selectedGenre, setSelectedGenre] = useState(null)
+  const client = useApolloClient()
+
+  useEffect(() => {
+    if(!booksQueryResult?.data) return
+    setBooks(booksQueryResult.data.allBooks)
+    // eslint-disable-next-line
+  }, [booksQueryResult.data]) 
+
+  const updateCacheWith = (addedBook) => {
+    const includedIn = (set, object) => 
+      set.map(p => p.title).includes(object.title)  
+
+    const updateBookCache = (queryName, genre, book) => {
+      // readQuery API will throw an error if the cache does not exist
+      try {
+      const dataInStore = genre ? client.readQuery({ query: queryName, variables: { genre } }) : client.readQuery({ query: queryName })
+
+      if (!includedIn(dataInStore.allBooks, book)) {
+        client.writeQuery({
+          query: queryName,
+          variables: genre ?  { genre } : null,
+          data: { allBooks : dataInStore.allBooks.concat(book) }
+        })
+      }
+      }
+      catch(e) {
+        console.log(e)
+      }
+    }  
+
+    updateBookCache(ALL_BOOKS, null, addedBook)
+    addedBook.genres.forEach(genre => updateBookCache(ALL_BOOKS_WITH_GENRE, genre, addedBook))
+  }
   
   useSubscription(BOOK_ADDED, {
     onSubscriptionData: ({ subscriptionData }) => {
-      alert(`${subscriptionData.data.bookAdded.title} was added!`)
+      // easy solution
+      // client.reFetchObservableQueries(true)
+      updateCacheWith(subscriptionData.data.bookAdded)
     }
   })
 

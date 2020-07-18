32c1be2b7d3ce986e54dce5f878dfc788ee905a2
endre.berki <gonosztaviszorny@gmail.com>
2020-07-18 12:28:56 +0200

    9.9 - Add Cors, modify ping endpoint

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 const app = express();
+import cors from 'cors';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
